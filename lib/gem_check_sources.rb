c8fc9fc6abcce7d402266034bc1a07c79e60f914
Britt Crawford <britt.v.crawford@gmail.com>
2010-04-19 19:31:03 -0700

    Renamed everything

---
+++ lib/gem_check_sources.rb
@@ -0,0 +1,2 @@
+require 'gem_check_sources/sources'
+require 'gem_check_sources/commands/manage_sources_command'
--- lib/gem_manage_sources.rb
+++
@@ -1,2 +0,0 @@
-require 'gem_manage_sources/sources'
-require 'gem_manage_sources/commands/manage_sources_command'
