a594cba5fbdae11d2db84efa4168ea5c152864b7
Britt Crawford <britt.v.crawford@gmail.com>
2010-04-19 20:25:55 -0700

    Rewrote specs; lots of yellow

--- lib/gem_check_sources/commands/check_sources_command.rb
+++ lib/gem_check_sources/commands/check_sources_command.rb
@@ -11,24 +11,8 @@ module Gem
       end
       
       def initialize
-        super('manage_sources', 'Manage the sources RubyGems use to search for gems. (USE INSTEAD OF: sources)')
-        defaults.merge!(:sources_to_add => [], :sources_to_remove => [], :check_sources? => false, :init? => false, :verbose? => false)
-        
-        add_option('-a', '--add SOURCE_URL', 'Add a gem source') do |value, options|
-          options[:sources_to_add] << value
-        end
-        
-        add_option('-r', '--remove SOURCE_URL', 'Remove a gem source') do |value, options|
-          options[:sources_to_remove] << value
-        end
-        
-        add_option('-c', '--check', 'Check gem sources. This will add or remove sources depending on availability.') do |value, options|
-          options[:check_sources?] = true
-        end
-        
-        add_option('-i', '--init', 'Generate the sources.yml file from your existing gem sources.') do |value, options|
-          options[:init?] = true
-        end
+        super('check_sources', 'Check which gem sources are currently available.')
+        defaults.merge!(:sources_to_add => [], :sources_to_remove => [], :verbose? => false)
       end
       
       def execute
--- spec/gem_check_sources/commands/check_sources_command_spec.rb
+++ spec/gem_check_sources/commands/check_sources_command_spec.rb
@@ -3,105 +3,25 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 describe Gem::Commands::CheckSourcesCommand do
   include Gem::Sources
   
-  before(:each) do
-    @test_sources_file = Dir.tmpdir + "/test_gem_sources.yml"
-    @command = Gem::Commands::CheckSourcesCommand.new
-    Gem::Commands::ManageSourcesCommand.stub!(:sources_file).and_return(@test_sources_file)
-  end
-  
-  after(:each) do
-    File.delete(@test_sources_file) if File.exist?(@test_sources_file)
-  end
-  
-  describe "options" do
-    before(:each) do
-      @command.when_invoked {}
-    end
-    
-    describe "add" do  
-      it "should be -a, --add" do
-        @command.invoke("-a", "http://gems.example.com")
-        @command.invoke("--add", "http://gems.example.com")
-        
-        @command.options[:args].should be_empty
-      end
-      
-      it "should add the given source to :sources_to_add" do
-        @command.invoke("-a", "http://gems.example.com")
-        @command.options[:sources_to_add].should include("http://gems.example.com")
-      end
-    end
-    
-    describe "remove" do  
-      it "should be -r, --remove" do
-        @command.invoke("-r", "http://gems.example.com")
-        @command.invoke("--remove", "http://gems.example.com")
-        
-        @command.options[:args].should be_empty
-      end
-      
-      it "should add the given source to :sources_to_add" do
-        @command.invoke("-r", "http://gems.example.com")
-        @command.options[:sources_to_remove].should include("http://gems.example.com")
-      end
-    end
-    
-    describe "check" do  
-      it "should be -c, --check" do
-        @command.invoke("-c")
-        @command.invoke("--check")
-        
-        @command.options[:args].should be_empty
-      end      
-      
-      it "check_sources? should be true" do
-        @command.invoke("-c")
-    
-        @command.options[:check_sources?].should be_true
-      end
-    end
-    
-    describe "init" do  
-      it "should be -i, --init" do
-        @command.invoke("-i")
-        @command.invoke("--init")
-        
-        @command.options[:args].should be_empty
-      end  
-      
-      it "init? should be true" do
-        @command.invoke("-i")
-        
-        @command.options[:init?].should be_true
-      end    
-    end    
-  end
+  # before(:each) do
+  #   @test_sources_file = Dir.tmpdir + "/test_gem_sources.yml"
+  #   @command = Gem::Commands::CheckSourcesCommand.new
+  #   Gem::Commands::ManageSourcesCommand.stub!(:sources_file).and_return(@test_sources_file)
+  # end
+  # 
+  # after(:each) do
+  #   File.delete(@test_sources_file) if File.exist?(@test_sources_file)
+  # end
 
-  describe "init" do
-    context "when there is no existing sources file" do
-      before(:each) do
-        Gem::Commands::CheckSourcesCommand.stub!(:sources_file).and_return(@test_sources_file)
-        File.exist?(@test_sources_file).should be_false
-
-        @sources = ['http://active.example.com','http://inactive.example.com']
-        @list = Gem::Sources::List.new(:unchecked => @sources)
-
-        @list.stub!(:source_available?) do |source|
-          source == 'http://active.example.com'
-        end
-        
-        Gem::Sources::List.stub!(:new).and_return(@list)
-
-        @command.stub!(:currently_loaded_sources).and_return(@sources)
-        @command.stub!(:sources).and_return(@list)
-        @command.invoke('-i')
-      end
-      
-      it "should create ~/.gem/ruby/sources.yml" do        
+  describe "check" do
+    context "when the sources file does not exist" do
+      it "should create ~/.gem/ruby/sources.yml" do   
+        pending     
         File.exist?(@test_sources_file).should be_true
       end
       
       it "should add all of the existing sources" do
+        pending
         sources = Gem::Sources::List.load_file(@test_sources_file)
         @sources.each do |source|
           sources.all.should include(source)
@@ -109,47 +29,37 @@ describe Gem::Commands::CheckSourcesCommand do
       end
             
       it "should add unavailable sources to the inactive list" do
+        pending
         sources = Gem::Sources::List.load_file(@test_sources_file)
         sources.active.should == ['http://active.example.com']
       end
       
       it "should add available sources to the active list" do
+        pending
         sources = Gem::Sources::List.load_file(@test_sources_file)
         sources.inactive.should == ['http://inactive.example.com']
       end
     end
-  end
-
-  describe "check" do
-    before(:each) do
-      @sources = ['http://active.example.com','http://inactive.example.com']
-      @list = Gem::Sources::List.new(:unchecked => @sources)
-      
-      @list.stub!(:sync)
-      @list.stub!(:source_available?) do |source|
-        source == 'http://active.example.com'
+    
+    context "when the sources file already exists" do    
+      it "should load the sources list from ~/.gem/ruby/sources.yml" do
+        pending
       end
-      @list.verify
-      @list.dump(@test_sources_file)
       
-      @sources << 'http://another.example.com'
-      @command.stub!(:currently_loaded_sources).and_return(@sources)
-      @command.stub!(:sources).and_return(@list)
-      
-      @command.invoke('-c')
-      @list = Gem::Sources::List.load_file(@test_sources_file)    
-    end
-    
-    it "should should add any new sources that have been added using 'gem source add'" do
-      @list.all.should include('http://another.example.com')
-    end
-    
-    it "should add unavailable sources to the inactive list" do 
-      @list.inactive.sort.should == ['http://inactive.example.com', 'http://another.example.com'].sort
-    end
-    
-    it "should add available sources to the active list" do
-      @list.active.should == ['http://active.example.com']
+      it "should should add any new sources that have been added using 'gem source add'" do
+        pending
+        @list.all.should include('http://another.example.com')
+      end
+
+      it "should add unavailable sources to the inactive list" do 
+        pending
+        @list.inactive.sort.should == ['http://inactive.example.com', 'http://another.example.com'].sort
+      end
+
+      it "should add available sources to the active list" do
+        pending
+        @list.active.should == ['http://active.example.com']
+      end
     end
   end
 end
